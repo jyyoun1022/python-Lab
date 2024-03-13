12ecd0e1653bb135881693953272ea4c53710b26
jyyoun <jyyoun@aistudio.co.kr>
2024-03-13 20:20:52 +0900

    function Lab

---
+++ start/lab/function/function_lab.py
@@ -0,0 +1,10 @@
+a_list = [3, 4, 62, 27, 83, 956, 26, 58, 3, 78, 168, 64, 78]
+for i in range(1, 10):
+    print(i)
+
+    # range(a, b) 는 a 부터 b-1 까지
+def print_list(a):
+    for i in a:
+        print(i)
+
+# print_list(a_list)
