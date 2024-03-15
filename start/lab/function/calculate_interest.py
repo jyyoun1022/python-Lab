6895b97bbd0b6948e2f9e01206133e76de58768e
jyyoun <jyyoun@aistudio.co.kr>
2024-03-15 10:30:34 +0900

    calculate_interest function

---
+++ start/lab/function/calculate_interest.py
@@ -0,0 +1,29 @@
+# 직장인 A씨는 1년 동안 열심히 일해서
+# 연말에 성과급으로 천만 원을 받았습니다.
+# 연이율 3.875%(단리)인 고정금리 상품에 예금하려고 합니다.
+# 5년 동안 넣어두면 이자가 얼마 붙는지 계산
+
+# 이자 = 원금 * 이율 * 기간
+# 원리금 = 원금 (1 + (이율*시간))
+def interest():
+    deposit = int(10000000)
+    deposit = deposit + (deposit * 0.03875 * 5)
+    print(deposit)
+
+# interest()
+
+def simple_interest(p, r, t):
+    return p * r * t
+
+def simple_interest_mount(p, r, t):
+    return p * (1 + (r*t))
+
+if __name__ == '__main__':
+    print('Quiz1\n'
+          'ex 1 : ', simple_interest(int(10000000), 0.03875, 5), '\n'
+          'ex 2 : ', simple_interest(int(1100000), 0.05, 5/12), '\n',
+          '\nQuiz2\n',
+          'ex 1 : ', simple_interest_mount(int(10000000), 0.03875, 5), '\n',
+          'ex 2 : ', simple_interest_mount(int(1100000), 0.05, 5/12)
+          )
+
